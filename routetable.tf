880d8d1a4d19f503c7d11334ec4802257e9be8bd
root <root@ANGEL-PC.localdomain>
2026-01-20 23:17:44 +0000

    adding all files

---
+++ routetable.tf
@@ -0,0 +1,45 @@
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "private_db_assoc" {
+  subnet_id      = aws_subnet.private_db.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_monitoring_assoc" {
+  subnet_id      = aws_subnet.private_monitoring.id
+  route_table_id = aws_route_table.private.id
+}
